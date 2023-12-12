2ed8606a5dc4b20e8cf15de467446eb8088da1bb
Max de Ruiter <m.j.deruiter99@hotmail.com>
2023-12-12 17:15:40 +0100

    working on animations

--- src/assets/components/hero/Hero.jsx
+++ src/assets/components/hero/Hero.jsx
@@ -1,40 +1,70 @@
 import { useLayoutEffect, useRef } from "react";
 import "./Hero.css";
 
-import { gsap } from "gsap";
+import { gsap, Power1, Power4 } from "gsap";
+import SplitType from "split-type";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 function Hero() {
   const context = useRef(null);
   const image = useRef(null);
+  const background = useRef(null);
 
   gsap.registerPlugin(ScrollTrigger);
 
   useLayoutEffect(() => {
+    const ourText = new SplitType("h1", { types: "chars" });
+    const chars = ourText.chars;
+
     let ctx = gsap.context(() => {
+      const tl = gsap.timeline({ defaults: { duration: 1 } });
+      tl.add("image");
+      tl.fromTo(
+        image.current,
+        { y: "100%" },
+        { y: "0%", ease: Power1.easeInOut },
+        "image"
+      );
+      tl.fromTo(
+        background.current,
+        { y: "100%" },
+        { y: "0%", ease: Power1.easeInOut },
+        "image"
+      );
+      tl.fromTo(
+        chars,
+        {
+          y: 100,
+          opacity: 0,
+        },
+        {
+          y: 0,
+          opacity: 1,
+          stagger: 0.05,
+          duration: 1.5,
+          ease: Power4.easeOut,
+        }
+      );
       gsap.to("#hero__image", {
         scrollTrigger: {
           trigger: "#hero__image",
-          start: "top center",
-          end: "center top",
+          start: "-100% center",
+          end: "-50% top",
           scrub: true,
-          markers: true,
+          // markers: true,
         },
-        rotation: 8,
-        // // transformOrigin: "left 50%",
+        rotation: 4,
       });
-      // gsap.to("#hero__description__bg", {
-      //   scrollTrigger: {
-      //     trigger: "#hero__description__bg",
-      //     start: "top bottom",
-      //     end: "bottom bottom",
-      //     scrub: true,
-      //     markers: true,
-      //   },
-      //   lineHeight: 1.5,
-      //   visibility: "visible",
-      //   opacity: 1,
-      // });
+      gsap.to("#hero__description__bg", {
+        scrollTrigger: {
+          trigger: "#hero__description__bg",
+          start: "-100% center",
+          end: "-50% top",
+          scrub: true,
+          // markers: true,
+        },
+        rotation: -4,
+      });
     }, context);
 
     return () => ctx.revert();
@@ -43,12 +73,16 @@ function Hero() {
   return (
     <div ref={context}>
       <section id="hero__title">
-        <h1>
-          DRWD maakt <span>websites</span> voor de MKB-sector
+        <h1 id="title">
+          <span>DRWD maakt&nbsp;</span>
+          <span>
+            <span className="highlight">websites</span>&nbsp;voor&nbsp;
+          </span>
+          <span>de MKB-sector</span>
         </h1>
       </section>
       <div id="hero__image" ref={image} />
-      <section id="hero__description__bg" />
+      <section id="hero__description__bg" ref={background} />
       <section id="hero__description">
         <b>
           Met een website van drwd legt u de essentie van uw bedrijf vast in een
@@ -63,6 +97,7 @@ function Hero() {
           Laten we daarom snel contact met elkaar opnemen.
         </p>
       </section>
+      <section id="test" />
     </div>
   );
 }
--- src/assets/components/navbar/NavBar.jsx
+++ src/assets/components/navbar/NavBar.jsx
@@ -1,17 +1,53 @@
-import logo from "../../images/icon.svg"
+import { useLayoutEffect, useRef } from "react";
+import { gsap, Power1 } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-import "./NavBar.css"
+import "./NavBar.css";
+import logo from "../../images/icon.svg";
 
 function NavBar() {
-    return (
-        <nav>
-            <img src={logo} alt="De Ruiter Webdevelopment Logo" />
-            <div>
-                <button>Contact</button>
-                <button className="secondary">Menu</button>
-            </div>
-        </nav>
-    )
+  const context = useRef(null);
+  const nav = useRef(null);
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  useLayoutEffect(() => {
+    let ctx = gsap.context(() => {
+      const tl = gsap.timeline({ defaults: { duration: 1 } });
+      tl.fromTo(
+        nav.current,
+        { y: "-100%" },
+        { y: "0%", ease: Power1.easeInOut }
+      );
+      //   gsap.to("#nav", {
+      //     scrollTrigger: {
+      //       trigger: "#nav",
+      //       start: "200% top",
+      //       //   end: "200% top",
+      //       //   scrub: true,
+      //       markers: true,
+      //     },
+      //     // top: 0,
+      //     position: "fixed",
+      //     backgroundColor: "var(--text-white)",
+      //   });
+    }, context);
+
+    return () => ctx.revert();
+  }, []);
+  return (
+    <div ref={context}>
+      <nav id="nav" ref={nav}>
+        <img src={logo} alt="De Ruiter Webdevelopment Logo" />
+        <div>
+          <button className="primary">Contact</button>
+          <button className="secondary">
+            <p>Menu</p>
+          </button>
+        </div>
+      </nav>
+    </div>
+  );
 }
 
 export default NavBar;
