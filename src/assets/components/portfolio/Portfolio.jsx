5d10bf50b5a49c083258a84205fc2c53f0e7c31c
Max de Ruiter <m.j.deruiter99@hotmail.com>
2023-12-14 12:08:00 +0100

    start on portfolio

--- src/assets/components/portfolio/Portfolio.jsx
+++ src/assets/components/portfolio/Portfolio.jsx
@@ -3,6 +3,8 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 import "./Portfolio.css";
+import portfolio from "./data.json";
+import arrow from "../../images/arrow_down.svg";
 
 function Portfolio() {
   const context = useRef(null);
@@ -27,6 +29,22 @@ function Portfolio() {
     return () => ctx.revert();
   }, []);
 
+  const foldOut = () => {
+    let ctx = gsap.context(() => {
+      gsap.to(".portfolio__item", {
+        y: 0,
+        position: "relative",
+        width: "100%",
+      });
+      gsap.to(".portfolio__item__title", {
+        visibility: "visible",
+        opacity: 1,
+      });
+    }, context);
+
+    return () => ctx.revert();
+  };
+
   return (
     <div ref={context}>
       <section id="portfolio">
@@ -34,6 +52,26 @@ function Portfolio() {
           <span />
           Portfolio
         </h2>
+        {portfolio.map((item, index) => {
+          const { title, category, text, img } = item;
+          return (
+            <div id={title} key={index}>
+              <img src={img} className="portfolio__item__img" alt={title} />
+              <div className="portfolio__item" onMouseOver={foldOut}>
+                <div className="portfolio__item__title">
+                  <h3>{title}</h3>
+                  <h4>{category}</h4>
+                </div>
+                <img
+                  src={arrow}
+                  className="portfolio__item__title__arrow"
+                  alt="Expand"
+                />
+              </div>
+              <p>{text}</p>
+            </div>
+          );
+        })}
       </section>
     </div>
   );
