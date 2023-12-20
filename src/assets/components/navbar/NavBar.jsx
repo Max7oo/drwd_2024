08e80244bb4331cfb5ec6ca89b8f1de13d68e982
Max de Ruiter <m.j.deruiter99@hotmail.com>
2023-12-20 12:32:21 +0100

    work on navbar

--- src/assets/components/navbar/NavBar.jsx
+++ src/assets/components/navbar/NavBar.jsx
@@ -1,6 +1,8 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { gsap, Power1 } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { Observer } from "gsap/Observer";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
 
 import "./NavBar.css";
 import logo from "../../images/icon.svg";
@@ -8,37 +10,66 @@ import logo from "../../images/icon.svg";
 function NavBar() {
   const context = useRef(null);
   const nav = useRef(null);
-  const fixedNav = useRef(null);
+
+  const [openMenu, setOpenMenu] = useState(true);
 
   // https://codepen.io/Mamboleoo/pen/poLEKob
 
   gsap.registerPlugin(ScrollTrigger);
+  gsap.registerPlugin(Observer);
+  gsap.registerPlugin(ScrollToPlugin);
 
   useEffect(() => {
     let ctx = gsap.context(() => {
       const tl = gsap.timeline({ defaults: { duration: 1 } });
       tl.fromTo(
         nav.current,
-        { y: "-100%" },
-        { y: "0%", ease: Power1.easeInOut }
+        { marginTop: "-100%" },
+        { marginTop: "0%", ease: Power1.easeInOut }
       );
-      // gsap.to("#fixedNav", {
-      //   scrollTrigger: {
-      //     trigger: "#fixedNav",
-      //     start: "700% top",
-      //     //   end: "200% top",
-      //     //   scrub: true,
-      //     // markers: true,
-      //     toggleActions: "play none none reverse",
-      //   },
-      //   backgroundColor: "#fff",
-      //   marginTop: 0,
-      //   position: "fixed",
-      // });
+      Observer.create({
+        target: window,
+        type: "scroll",
+        tolerance: 50,
+        onUp: () => nav.current.classList.remove("is-hidden"),
+        onDown: () => nav.current.classList.add("is-hidden"),
+      });
     }, context);
 
     return () => ctx.revert();
   }, []);
+
+  const menu = () => {
+    let ctx = gsap.context(() => {
+      if (openMenu) {
+        gsap.to("#menu", {
+          x: 0,
+          visibility: "visible",
+          opacity: 1,
+        });
+        setOpenMenu(!openMenu);
+      } else {
+        gsap.to("#menu", {
+          x: 100,
+          visibility: "hidden",
+          opacity: 0,
+        });
+        setOpenMenu(!openMenu);
+      }
+    }, context);
+
+    return () => ctx.revert();
+  };
+
+  const scrollToItem = (item) => {
+    console.log(item);
+    gsap.to(window, {
+      duration: 1,
+      scrollTo: { y: `#${item}`, offsetY: 50 },
+      ease: "Power1.easeInOut",
+    });
+    menu();
+  };
 
   return (
     <div ref={context}>
@@ -46,20 +77,16 @@ function NavBar() {
         <img src={logo} alt="De Ruiter Webdevelopment Logo" />
         <div>
           {/* <button className="primary">Contact</button> */}
-          <button className="secondary">
+          <button className="secondary" onClick={() => menu()}>
             <p>Menu</p>
           </button>
         </div>
       </nav>
-      {/* <nav id="fixedNav" ref={fixedNav}>
-        <img src={logo} alt="De Ruiter Webdevelopment Logo" />
-        <div>
-          <button className="primary">Contact</button>
-          <button className="secondary">
-            <p>Menu</p>
-          </button>
-        </div>
-      </nav> */}
+      <div id="menu">
+        <button onClick={() => scrollToItem("portfolio")}>Portfolio</button>
+        <button onClick={() => scrollToItem("services")}>Services</button>
+        <button onClick={() => scrollToItem("contact")}>Contact</button>
+      </div>
     </div>
   );
 }
